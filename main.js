f07f9328172f40855b7cbb734f03d1794227499f
Guayabas <guayabas0399@gmail.com>
2022-05-29 20:38:28 -0600

    finished

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 let number = 0
 const unit = document.querySelector('#unit');
+const length = document.querySelector('.sub-para-length');
+const volume = document.querySelector('.sub-para-volume');
+const mass = document.querySelector('.sub-para-mass');
 
 function ask(){
     number = prompt("What number do you wish to use?");
+    //console.log("Si paso")
     unit.textContent = number;
+    let mf = Number(number * 3.281).toFixed(3);
+    let fm = Number(number / 3.281).toFixed(3);
+    length.textContent = number + " meters = " + mf + " feet | " + number + " feet = " + fm + " meters";
+    let lg = Number(number / 3.785).toFixed(3);
+    let gl = Number(number * 3.785).toFixed(3);
+    volume.textContent = number + " liters = " + lg + " gallons | " + number + " gallons = " + gl + " liters";
+    let kp = Number(number * 2.205).toFixed(3);
+    let pk = Number(number / 2.205).toFixed(3);
+    mass.textContent = number + " kilograms = " + kp + " pounds | " + number + " pounds = " + pk + " kilograms";
 }
